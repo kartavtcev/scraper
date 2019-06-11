34ae326afcc4a1e6ead6907fee38ffc1ff6344cd
kartavtcev <kartavtcev.sergei@gmail.com>
2019-06-11 16:24:22 +0300

    GraphQL Server compiled. No DB yet.

--- src/main/scala/example/Server.scala
+++ src/main/scala/example/Server.scala
@@ -1,9 +1,50 @@
 package example
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect._
+import cats.implicits._
+import example.domain.News._
+import example.infrastructure.endpoint._
+import example.infrastructure.repository._
+import org.http4s.server.blaze.BlazeServerBuilder
+
+import scala.concurrent.ExecutionContext
 
 // TODO: config, db later - hardcode now & check that sangria works...
 
 object Server extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = ???
+  def createInfractructure[F[_] : ContextShift : ConcurrentEffect : Timer] : NewsService[F] = {
+    //for{
+    //_ <- IO("test")
+    val newsRepo = NewsRepositoryInMemoryInterpreter[F]()
+    val newsValidation = NewsValidationInterpreter[F](newsRepo)
+    val newsService = NewsService[F](newsRepo, newsValidation)
+    //} yield newsService
+    newsService
+  }
+
+  def run(args: List[String]): IO[ExitCode] = {
+    implicit val ec = ExecutionContext.global
+
+    BlazeServerBuilder[IO]
+      .bindHttp(8080, "0.0.0.0")
+      .withHttpApp(GraphQLEndpoints.graphQLEndpoint(createInfractructure[IO]))
+      .resource
+      .use(_ => IO.never).as(ExitCode.Success)
+  }
+
+  /*
+  def createServer(newsService: NewsService[IO])(implicit ec: ExecutionContext, cs: ContextShift[IO]) = {
+    val server =
+    BlazeServerBuilder[IO]
+      .bindHttp(8080, "0.0.0.0")
+      .withHttpApp(GraphQLEndpoints.graphQLEndpoint(newsService))
+      .resource
+    server
+  }
+*/
+
+/*
+  def run(args: List[String]): IO[ExitCode] =
+    createServer(createInfractructure[IO]).use(_ => IO.never).as(ExitCode.Success)
+*/
 }
