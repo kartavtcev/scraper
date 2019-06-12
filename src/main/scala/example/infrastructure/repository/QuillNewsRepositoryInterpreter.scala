a4d694879e0e59e447164aafbe427905fd7bf509
kartavtcev <kartavtcev.sergei@gmail.com>
2019-06-12 22:10:02 +0300

    Quill repo implementation with ContextShift. No pagination in the DB yet.

--- src/main/scala/example/infrastructure/repository/QuillNewsRepositoryInterpreter.scala
+++ src/main/scala/example/infrastructure/repository/QuillNewsRepositoryInterpreter.scala
@@ -1,46 +1,57 @@
 package example.infrastructure.repository
 
 import cats.effect._
+import cats.implicits._
+
 import com.typesafe.config.ConfigFactory
 import example.config.DatabaseConfig
 import example.domain.News._
 import io.getquill._
 
-class QuillNewsRepositoryInterpreter[F[_] : Sync](dbConfig: DatabaseConfig) extends NewsRepositoryAlgebra[F] {
+class QuillNewsRepositoryInterpreter[F[_]](dbConfig: DatabaseConfig)(implicit F: Sync[F], cs: ContextShift[F])
+    extends NewsRepositoryAlgebra[F] {
 
-  // I'm using circe config, not Lightbend for the app.
-  val config = ConfigFactory.parseString(
-    s"""dataSourceClassName="${dbConfig.dataSourceClassName}",
+  // I'm using circe config, not Lightbend.
+  val config = ConfigFactory.parseString(s"""dataSourceClassName="${dbConfig.dataSourceClassName}",
       dataSource.url="${dbConfig.url}",
       dataSource.user=${dbConfig.user}""")
+  /*
+    The return type of performIO varies according to the context.
+    For instance, async contexts return Futures while JDBC returns values synchronously.
+   */
   lazy val ctx = new H2JdbcContext(SnakeCase, config)
   import ctx._
 
-  def selectAllLines: List[NewsItem] = {
-    val newsItemsQuery = quote {
+  object schema {
+    val headlines = quote {
       querySchema[NewsItem](
         "headlines",
         _.link -> "link",
         _.title -> "title"
       )
     }
-    val q = quote {
-      newsItemsQuery
-    }
-
-    ctx.run(q)
   }
 
+  private def insert(newsItem: NewsItem): Unit =
+    ctx.run(schema.headlines.insert(lift(newsItem)))
 
-  def create(newsItem: NewsItem): F[Unit] = ???
+  private def selectOne(link: String): Option[NewsItem] =
+    ctx.run(schema.headlines.filter(_.link == lift(link))) headOption
 
-  def get(link: String): F[Option[NewsItem]] = ???
+  // TODO: !MPRT: move pagination to DB sql to not select the entire DB over the network in PROD.
+  private def selectAllLines(pageSize: Int, offset: Int): List[NewsItem] =
+    ctx.run(schema.headlines).slice(offset, offset + pageSize)
 
-  def list(pageSize: Int, offset: Int): F[List[NewsItem]] = {
-    Sync[F].delay(selectAllLines)
-  }
+  def create(newsItem: NewsItem): F[Unit] =
+    cs.shift *> F.pure(insert(newsItem))
+
+  def get(link: String): F[Option[NewsItem]] =
+    cs.shift *> F.pure(selectOne(link))
+
+  def list(pageSize: Int, offset: Int): F[List[NewsItem]] =
+    cs.shift *> F.pure(selectAllLines(pageSize, offset))
 }
 
 object QuillNewsRepositoryInterpreter {
-  def apply[F[_]: Sync](dbConfig: DatabaseConfig) = new QuillNewsRepositoryInterpreter[F](dbConfig)
+  def apply[F[_]](dbConfig: DatabaseConfig)(implicit F: Sync[F], cs: ContextShift[F]) = new QuillNewsRepositoryInterpreter[F](dbConfig)
 }
